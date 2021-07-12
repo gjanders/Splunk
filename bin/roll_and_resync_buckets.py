ec6e69fa858ec37d533c650f14eb997fa362b716
gjanders <gareth.anderson03@gmail.com>
2021-07-12 17:10:25 +1000

    Update roll_and_resync_buckets.py

--- bin/roll_and_resync_buckets.py
+++ bin/roll_and_resync_buckets.py
@@ -1,9 +1,9 @@
+#run this with splunk cmd python3 or python3
 import requests
 import json
 base_url="https://localhost:8089"
 url = base_url + "/services/cluster/master/buckets?filter=meets_multisite_replication_count=false&output_mode=json&count=0"
 auth=('admin','changeme')
-requests.get(url)
 res=requests.get(url,auth=auth,verify=False)
 
 dict = json.loads(res.text)
@@ -11,11 +11,13 @@ dict = json.loads(res.text)
 roll_bucket_url = base_url + "/services/cluster/master/control/control/roll-hot-buckets"
 resync_bucket_url = base_url + "/services/cluster/master/control/control/resync_bucket_from_peer"
 for entry in dict['entry']:
-    site_count = len(entry['content']['rep_count_by_site'])
+    #print(entry['content'])
+    rep_count_by_site = entry['content']['rep_count_by_site']
+    site_count = len(rep_count_by_site)
     copy_count = 2
     if site_count == 1:
-        copy_count = list(entry['content']['rep_count_by_site'].values())[0]        
-    name = entry['name']    
+        copy_count = list(entry['content']['rep_count_by_site'].values())[0]
+    name = entry['name']
     if rep_count_by_site == 1 and copy_count < 2:
         print(f'{name} is only found on 1 site, rolling hot bucket')
         data = { 'bucket_id': name }
