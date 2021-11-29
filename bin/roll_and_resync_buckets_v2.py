ad9060a18ebace156713b307f47c8ad9d29660db
gjanders <gareth.anderson03@gmail.com>
2021-11-29 16:12:25 +1100

    Add files via upload

---
+++ bin/roll_and_resync_buckets_v2.py
@@ -0,0 +1,50 @@
+#run this with splunk cmd python3 or python3
+import requests
+import json
+import time
+import sys
+
+wait_for_seconds = 60 * 10
+
+base_url="https://localhost:8089"
+
+try:
+    with open('/opt/splunk/.password','r') as file:
+        password = file.readlines()[0].strip()
+except:
+    print("Unable to open password file")
+    sys.exit(2)
+
+# role with capabilities /opt/splunk/bin/splunk _internal call /services/authorization/roles -post:capabilities edit_indexer_cluster -post:capabilities list_indexer_cluster -post:name roll_buckets_automated -auth ... required
+auth=('roll_buckets_automated', password)
+
+url = base_url + "/services/cluster/manager/fixup?output_mode=json&count=0&level=replication_factor"
+res = requests.get(url,auth=auth,verify=False)
+
+dict = json.loads(res.text)
+print(f"status_code={res.status_code} on url={url}")
+
+roll_bucket_url = base_url + "/services/cluster/master/control/control/roll-hot-buckets"
+
+current_time = round(time.time())
+
+for entry in dict['entry']:
+    data_latest = entry['content']['latest']
+    data_initial = entry['content']['initial']
+    if data_latest['reason'].find("bucket hasn't rolled yet") != -1:
+        name = entry['name']
+        reason = data_latest['reason']
+        print(f"bucket={name} requires role due to {reason}")
+        bucket_timestamp = data_initial['timestamp']
+        diff = current_time - bucket_timestamp
+        print(diff)
+        if diff > wait_for_seconds:
+            print(f'bucket={name} requires role due to {reason}, and is beyond {wait_for_seconds} seconds')
+            data = { 'bucket_id': name }
+            print(f'requests.post("{roll_bucket_url}", data={data}, verify=False)')
+            res=requests.post(roll_bucket_url, auth=auth, data=data, verify=False)
+            print(res.status_code)
+            print(res.text)
+            if res.status_code != requests.codes.ok:
+                print(f'bucket={name} code={res.status_code} text={res.text}')
+    time.sleep(1)
