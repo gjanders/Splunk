fc43d083a88856b9d401f4a75012b654dec2a56a
gjanders <gareth.anderson03@gmail.com>
2022-09-20 10:19:24 +1000

    Update knowledge_obj_extraction_conffiles.py
    Added user directories as well, minor code cleanup

--- bin/knowledge_obj_extraction_conffiles.py
+++ bin/knowledge_obj_extraction_conffiles.py
@@ -1,10 +1,7 @@
-from subprocess import Popen, PIPE, check_output
-import re
 import logging
 from logging.config import dictConfig
 import argparse
 import os
-from configparser import RawConfigParser
 import csv
 import urllib.parse
 import sys
@@ -63,6 +60,8 @@ parser.add_argument('-app', help='Splunk app to extract the knowledge object fro
 parser.add_argument('-filterCSV', help='A CSV list to filter the names of the objects, lines should be type/name, or just the name if filtering without the "all" type', required=False)
 parser.add_argument('-debugMode', help='(optional) turn on DEBUG level logging (defaults to INFO)', action='store_true')
 parser.add_argument('-outputDir', help='Directory to output files to', required=True)
+parser.add_argument('-migrateUserDirs', help='Copy the $SPLUNK_HOME/etc/users/<app> as well into outputDir?', action='store_true')
+parser.add_argument('-outputAppName', help='Specify the app to be created in $outputDir/users/<appname>/config, defaults to the -app name passed in if not specified')
 
 args = parser.parse_args()
 
@@ -107,18 +106,18 @@ def parse_single_conf_file(conf_file, app, splunk_type, filter_type, filter_list
     return string_res_list
 
 # parse all conf files for an app
-def parse_conf_files(splunkhome, splunk_type, app, filter_list, filter_type):
+def parse_conf_files(app_dir, splunk_type, app, filter_list, filter_type):
     # these types do not have .conf files as such
     if splunk_type == "lookups" or splunk_type == "views":
         logger.info("Skipping views/lookups in conf file reading, this is only required for metadata")
         return [], []
 
     # attempt to parse the default/<type>.conf files
-    conf_file = f"{splunkhome}/etc/apps/{app}/default/{splunk_type}.conf"
+    conf_file = f"{app_dir}/default/{splunk_type}.conf"
     string_res_list_default = parse_single_conf_file(conf_file, app, splunk_type, filter_type, filter_list)
 
     # attempt to parse the local/<type>.conf files
-    conf_file = f"{splunkhome}/etc/apps/{app}/local/{splunk_type}.conf"
+    conf_file = f"{app_dir}/local/{splunk_type}.conf"
     string_res_list = parse_single_conf_file(conf_file, app, splunk_type, filter_type, filter_list)
 
     return string_res_list_default, string_res_list
@@ -165,23 +164,19 @@ if args.type=='all':
 else:
     splunk_type = [ args.type ]
 
-# Create the directories we will need
-if not os.path.isdir(args.outputDir):
-    os.mkdir(args.outputDir)
-
-local_dir = f'{args.outputDir}/local'
+local_dir = f'{args.outputDir}/apps/local'
 if not os.path.isdir(local_dir):
-    os.mkdir(local_dir)
+    os.makedirs(local_dir, exist_ok=True)
 
-metadata_dir = f'{args.outputDir}/metadata'
+metadata_dir = f'{args.outputDir}/apps/metadata'
 if not os.path.isdir(metadata_dir):
     os.mkdir(metadata_dir)
 
-default_dir = f'{args.outputDir}/default'
+default_dir = f'{args.outputDir}/apps/default'
 if not os.path.isdir(default_dir):
     os.mkdir(default_dir)
 
-lookups_dir = f'{args.outputDir}/lookups'
+lookups_dir = f'{args.outputDir}/apps/lookups'
 
 ################
 #
@@ -219,9 +214,23 @@ if args.filterCSV:
 # keep a list of filtered types for when we parse the config
 filter_type = list(filter_type.keys())
 
+user_dirs_to_check = []
+if args.migrateUserDirs:
+    user_dirs = os.listdir(f"{args.splunkhome}/etc/users")
+    logger.debug(f"Checking user_dirs={user_dirs}")
+    for user in user_dirs:
+        a_user_dir = f"{args.splunkhome}/etc/users/{user}"
+        if not os.path.isdir(a_user_dir):
+            continue
+        if args.app in os.listdir(a_user_dir):
+            user_dir_found = f"{a_user_dir}/{args.app}"
+            logger.debug(f"Appending directory user_dir_found={user_dir_found}")
+            user_dirs_to_check.append(user_dir_found)
+
 # each splunk_type is a separate configuration file
 for a_type in splunk_type:
-    string_res_list_default, string_res_list = parse_conf_files(args.splunkhome, a_type, args.app, filter_list, filter_type)
+    app_dir = f"{args.splunkhome}/etc/apps/{args.app}"
+    string_res_list_default, string_res_list = parse_conf_files(app_dir, a_type, args.app, filter_list, filter_type)
 
     if len(string_res_list) > 0:
         with open(f'{local_dir}/{a_type}.conf', 'w') as f:
@@ -233,31 +242,65 @@ for a_type in splunk_type:
             for line in string_res_list_default:
                 f.write(f"{line}")
 
+    # deal with user-level objects too
+    if args.migrateUserDirs:
+        for a_dir in user_dirs_to_check:
+            string_res_list_default, string_res_list = parse_conf_files(a_dir, a_type, args.app, filter_list, filter_type)
+            if len(string_res_list) > 0:
+                user_dir = os.path.basename(os.path.dirname(a_dir))
+                if args.outputAppName:
+                    output_app_name = args.outputAppName
+                else:
+                    output_app_name = args.app
+                dest_dir = f"{args.outputDir}/users/{user_dir}/{output_app_name}"
+                os.makedirs(f"{dest_dir}/local", exist_ok=True)
+                with open(f'{dest_dir}/local/{a_type}.conf', 'w') as f:
+                    for line in string_res_list:
+                        f.write(f"{line}")
+
 logger.info("knowledge object extraction ends")
 
 logger.info("Metadata extraction begins")
 
+# there is only 1 default metadata file for all types of config
 default_meta_file = f"{args.splunkhome}/etc/apps/{args.app}/metadata/default.meta"
 if not os.path.isfile(default_meta_file):
     logger.warning(f"file={default_meta_file} does not exist skipping this file")
 else:
-    # there is only 1 default metadata file for all types of config
     string_res_list = parse_metadata_file(default_meta_file, args.app, splunk_type, filter_type, filter_list_encoded)
     if len(string_res_list) > 0:
         with open(f'{metadata_dir}/default.meta', 'w') as f:
             for line in string_res_list:
                 f.write(f"{line}")
 
+
+# there is only 1 local metadata file for all types of config
 local_meta_file = f"{args.splunkhome}/etc/apps/{args.app}/metadata/local.meta"
 if not os.path.isfile(local_meta_file):
     logger.warning(f"file={local_meta_file} does not exist skipping this file")
 else:
-    # there is only 1 local metadata file for all types of config
     string_res_list = parse_metadata_file(local_meta_file, args.app, splunk_type, filter_type, filter_list_encoded)
     if len(string_res_list) > 0:
         with open(f'{metadata_dir}/local.meta', 'w') as f:
             for line in string_res_list:
                 f.write(f"{line}")
+
+# if we are migrating user directories we may need user metadata too
+if args.migrateUserDirs:
+    for a_dir in user_dirs_to_check:
+        user_dir = os.path.basename(os.path.dirname(a_dir))
+        string_res_list = parse_metadata_file(f"{a_dir}/metadata/local.meta", args.app, splunk_type, filter_type, filter_list_encoded)
+
+        if len(string_res_list) > 0:
+            if args.outputAppName:
+                output_app_name = args.outputAppName
+            else:
+                output_app_name = args.app
+            metadata_dir = f"{args.outputDir}/users/{user_dir}/{output_app_name}/metadata"
+            os.makedirs(metadata_dir, exist_ok=True)
+            with open(f"{metadata_dir}/local.meta", 'w') as f:
+                for line in string_res_list:
+                    f.write(f"{line}")
 
 logger.info("Metadata extraction ends")
 
@@ -324,3 +367,35 @@ for type in splunk_type:
             if not os.path.isdir(nav_dir):
                 os.makedirs(nav_dir, exist_ok=True)
             shutil.copy2(local_nav, nav_dir)
+
+# if we are migrating user directories we may need other files as well
+if args.migrateUserDirs:
+    for a_dir in user_dirs_to_check:
+        user_dir = os.path.basename(os.path.dirname(a_dir))
+
+        if args.outputAppName:
+            output_app_name = args.outputAppName
+        else:
+            output_app_name = args.app
+
+        for type in splunk_type:
+            if type == "datamodels":
+                datamodels_src = f"{a_dir}/local/data/models"
+                datamodels_dest = f"{args.outputDir}/users/{user_dir}/{output_app_name}/local/data/models"
+                logger.debug(f"datamodels_src={datamodels_src} datamodels_dest={datamodels_dest}")
+                find_and_copy_files(datamodels_src, datamodels_dest, "datamodels", ".json", filter_type, filter_list)
+            elif type == "lookups":
+                lookups_src = f"{a_dir}/lookups"
+                lookups_dest = f"{args.outputDir}/users/{user_dir}/{output_app_name}/lookups"
+                logger.debug(f"src={lookups_src} dest={lookups_dest}")
+                find_and_copy_files(lookups_src, lookups_dest, "lookups", ".csv", filter_type, filter_list)
+            elif type == "panels":
+                panels_src = f"{a_dir}/local/data/ui/panels"
+                panels_dst = f"{args.outputDir}/users/{user_dir}/{output_app_name}/local/data/ui/panels"
+                logger.debug(f"src={panels_src} dest={panels_dst}")
+                find_and_copy_files(panels_src, panels_dst, "panels", ".xml", filter_type, filter_list)
+            elif type=="views":
+                views_src = f"{a_dir}/local/data/ui/views"
+                views_dst = f"{args.outputDir}/users/{user_dir}/{output_app_name}/local/data/ui/views"
+                logger.debug(f"src={views_src} dest={views_dst}")
+                find_and_copy_files(views_src, views_dst, "views", ".xml", filter_type, filter_list)
