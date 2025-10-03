9f040230f69ca538d343f708defa0ae93880259f
Gareth Anderson <gareth.anderson03@gmail.com>
2025-10-03 15:28:52 +1000

    Update check_if_restart_required.sh
    Adding savedsearches.conf as the simple trigger is not there in 9.4.4

--- bin/check_if_restart_required.sh
+++ bin/check_if_restart_required.sh
@@ -40,7 +40,7 @@ reload_conf=`grep "reload.*= simple" /opt/splunk/etc/system/default/app.conf | c
 # this works in my environment may require further testing...triggers with access_endpoints sometimes works but it depends what was in the config
 # for example reload.distsearch          = access_endpoints /search/distributed/bundle-replication-files
 # if the config file for distsearch contains [replicationBlacklist] then it won't require a restart, but if contains [replicationSettings] it may require a restart...
-reload_conf="${reload_conf} authentication.conf authorize.conf collections.conf indexes.conf messages.conf props.conf transforms.conf web.conf workload_pools.conf workload_rules.conf workload_policy.conf inputs.conf restmap.conf setup.xml"
+reload_conf="${reload_conf} authentication.conf authorize.conf collections.conf indexes.conf messages.conf props.conf transforms.conf web.conf workload_pools.conf workload_rules.conf workload_policy.conf inputs.conf restmap.conf setup.xml savedsearches.conf"
 
 restart_required_any="False"
 
@@ -118,3 +118,4 @@ do
 done
 
 echo "${date} restart_required=${restart_required_any}"
+
